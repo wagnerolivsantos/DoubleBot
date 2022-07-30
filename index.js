da0dd332ce8560a3164e727ab78c8c1d1813adfa
Wagner Oliveira <wagnerolivsantosdev@gmail.com>
2022-07-30 20:13:51 -0300

    🚀 Checking Connection status

--- index.js
+++ index.js
@@ -6,18 +6,42 @@ const puppeteer = require("puppeteer");
 
 const url = process.env.SITE;
 
-(async () => {
-  try {
-    const browser = await puppeteer.launch({
-      headless: false,
-      args: ["--start-maximized"],
-    });
+function consoleMessage(message) {
+  console.log(message);
+}
 
+(async () => {
+  const browser = await puppeteer.launch({
+    headless: false,
+    args: ["--start-maximized"],
+  });
+
+  let counter = 0;
+
+  try {
     const page = await browser.newPage();
     await page.setViewport({ width: 1366, height: 768 });
     await page.goto(url);
 
-    await browser.close();
+    const robot = setInterval(async () => {
+      let connectionStatus = await page.evaluate(() => {
+        return navigator.onLine;
+      });
+
+      if (connectionStatus) {
+        consoleMessage("Online");
+      } else {
+        counter++;
+
+        page.reload();
+
+        if (counter == process.env.MAXIMUM_RECONNECTION) {
+          consoleMessage("Error: Offline");
+          clearInterval(robot);
+          await browser.close();
+        }
+      }
+    }, 5000);
   } catch {
     await browser.close();
   }
